5a6776973f2d68d25ba286fa6d96f57279065d00
kujyp <pjo901018@gmail.com>
2018-06-02 01:44:20 +0900

    하아..

--- app/src/main/java/com/asuscomm/yangyinetwork/mapplayground/MainActivity.kt
+++ app/src/main/java/com/asuscomm/yangyinetwork/mapplayground/MainActivity.kt
@@ -2,11 +2,41 @@ package com.asuscomm.yangyinetwork.mapplayground
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.SyncStateContract.Helpers.update
+import android.content.pm.PackageManager
+import android.content.pm.PackageInfo
+import android.util.Base64
+import android.util.Log
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.activity_main.*
+import net.daum.mf.map.api.MapPoint
+import net.daum.mf.map.api.MapView
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        getAppkey()
+
+        map_view.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(37.53737528, 127.00557633), true)
     }
+//    private fun getAppkey() {
+//        try {
+//            val info = packageManager.getPackageInfo("com.asuscomm.yangyinetwork.mapplayground", PackageManager.GET_SIGNATURES)
+//            for (signature in info.signatures) {
+//                val md = MessageDigest.getInstance("SHA")
+//                md.update(signature.toByteArray())
+//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+//            }
+//        } catch (e: PackageManager.NameNotFoundException) {
+//            e.printStackTrace()
+//        } catch (e: NoSuchAlgorithmException) {
+//            e.printStackTrace()
+//        }
+//    }
 }
